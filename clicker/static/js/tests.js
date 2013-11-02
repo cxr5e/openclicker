62d60fd7bbb2bc7a80448c6a6bbdc5e154892511
Ryan Lam <darth.ryking@gmail.com>
2013-11-02 17:23:07 -0400

    added some legal stuff I think...

--- clicker/static/js/tests.js
+++ clicker/static/js/tests.js
@@ -1,8 +1,33 @@
-isEven(val) {
-    return val % 2 === 0; 
-}
+/*
 
-test('isEven()', function() {
-    ok(isEven(0), 'Zero is an even number'); 
-    ok(isEven(-4), 'Negative four is an even number'); 
+Openclicker - an open source clicker game.
+Copyright (C) 2013  Cheryl Yang and Ryan Lam
+Contact: cheriexryan@hellokitty.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+test('incrementProjects', function() {
+    // First call should increment project once.
+    result = incrementProjects(); 
+    ok(result === 1, 'First call was successful'); 
+    
+    // Second call should increment project again. 
+    result = incrementProjects(); 
+    ok(result === 2, 'Second call was successful'); 
+    
+    result = incrementProjects(); 
+    ok(result === 3, 'Third call was successful'); 
     })
--- static/js/clicker.js
+++ static/js/clicker.js
@@ -1,6 +1,32 @@
+/*
+
+Openclicker - an open source clicker game.
+Copyright (C) 2013  Cheryl Yang and Ryan Lam
+Contact: cheriexryan@hellokitty.com
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
+// This variable represents the current number of projects the player has made.
 projects = 0; 
+
+// This function will increment projects by 1 and update the page to reflect this change.
+// Returns project for testing.
 function incrementProjects() {
     projects++; 
     countDiv = document.getElementById('count'); 
     countDiv.innerHTML = projects+" open source projects made!"
+    return projects; 
 }
